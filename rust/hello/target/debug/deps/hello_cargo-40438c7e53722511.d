e9e9235ff9686abe6bc71daa5eb1786c8abfeedc
102300671 <jy2193807541.gmail.com>
2025-03-14 01:22:00 +0000

    更新 Rust 项目，调整 Cargo.lock 版本并添加新生成的二进制文件

---
+++ rust/hello/target/debug/deps/hello_cargo-40438c7e53722511.d
@@ -0,0 +1,5 @@
+/root/code/rust/hello/target/debug/deps/hello_cargo-40438c7e53722511: src/main.rs
+
+/root/code/rust/hello/target/debug/deps/hello_cargo-40438c7e53722511.d: src/main.rs
+
+src/main.rs:
---
+++ rust/sum/target/debug/deps/sum-c54b07d68092a005.d
@@ -0,0 +1,5 @@
+/root/code/rust/sum/target/debug/deps/sum-c54b07d68092a005: src/main.rs
+
+/root/code/rust/sum/target/debug/deps/sum-c54b07d68092a005.d: src/main.rs
+
+src/main.rs:
